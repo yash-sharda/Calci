6d3efc2b1e8a2292c31857cbfe50dcdccb567fd6
yash <yashsharda606@gmail.com>
2025-04-04 00:27:17 +0530

    integrated subtract function

--- simple_calc.cpp
+++ simple_calc.cpp
@@ -8,8 +8,14 @@ int add(int a, int b){
   return a+b;
 }
 
+int subtract(int a, int b){
+  return a-b;
+}
+
 int main() {
   int a=4, b=5;
   cout<<add(a,b)<<endl;
+  cout<<subtract(a,b)<<endl;
+
   return 0;
 }
